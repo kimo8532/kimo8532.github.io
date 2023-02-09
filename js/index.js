4b5415ced99318e2ad71931cb9f69089625776fb
kimo8532 <nemet.vinko@gmail.com>
2023-02-09 09:16:25 +0100

    temporary fix

--- js/index.js
+++ js/index.js
@@ -501,7 +501,6 @@ function generatePDF() {
     // Choose the element that our invoice is rendered in.
     html2pdf().set(opt).from(element).save().then(function(e)
     {
-      document.getElementById("invoice").setAttribute('hidden','');
     });
   }
 /*<div class="modal fade" id="invoice">
